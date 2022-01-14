f771506e4eaad3e575008527abbd66e754a71122
Gustavo Santos <gts_br@yahoo.com.br>
2022-01-14 10:08:21 -0300

    Add Departments View/Model/Controller

---
+++ SalesWebMVC/Controllers/DepartmentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Teste 1" });
+            list.Add(new Department { Id = 2, Name = "Teste 2" });
+
+            return View(list);
+        }
+    }
+}
