3cda17320fb024644781c2d73b473f26d8449dcf
Gustavo Torres <vforce19@LIVE.COM>
2022-01-19 14:43:24 -0300

    App locale, number and date formatting

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMVC.Data;
 using SalesWebMVC.Services;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,11 +24,17 @@ builder.Services.BuildServiceProvider();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
-
+var enUS = new CultureInfo("en-US");
+var localizationOptions = new RequestLocalizationOptions()
+{
+    DefaultRequestCulture = new RequestCulture(enUS),
+    SupportedCultures = new List<CultureInfo> { enUS },
+    SupportedUICultures = new List<CultureInfo> { enUS },
+};
+app.UseRequestLocalization(localizationOptions);
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
-{    
+{
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
